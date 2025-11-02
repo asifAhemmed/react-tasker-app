10823857b95861ec1d9b492ae4571391bfd0876d
asif ahemmed <asifahemmed0@gmail.com>
2025-11-02 14:35:03 +0600

    header, hero section & footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
 
 
 const App = () => {
   return (
-    <div>
-      Welcome to tasker app
-    </div>
+    <>
+      <Header />
+      <HeroSection />
+      <Footer />
+    </>
   );
 };
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer class="py-6 md:py-8">
+      <div class="container mx-auto">
+        <p class="text-center text-xs text-[#6A6A6A] lg:text-sm">
+          Copyright ©{new Date().getFullYear()} | All rights reserved by Asif Ahemmed
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

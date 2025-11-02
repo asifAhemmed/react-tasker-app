eb6027cb927c32e553526303d81e274a0535f001
asif ahemmed <asifahemmed0@gmail.com>
2025-11-02 15:08:10 +0600

    tasklists rendered dynamically

--- src/components/Task/TaskBoard.jsx
+++ src/components/Task/TaskBoard.jsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskLists from "./TaskLists";
 
 const TaskBoard = () => {
+    const [tasks, setTasks] = useState([{
+        id: crypto.randomUUID(),
+        title: "Learn React",
+        description: "Learning React is fun!",
+        tags: ["javaScript", "react", "frontEnd"],
+        priority: "medium",
+        isFavorite: false
+    }]);
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
         <SearchTask />
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskActions />
-          <TaskLists />
+          <TaskLists tasks={tasks} />
         </div>
       </div>
     </section>
